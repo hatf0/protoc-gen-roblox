feaaa2ed68ec3996098f00386f46a6b9a658b085
hatf0 <harrison@hat.fo>
2023-12-25 00:00:13 -0500

    avoid copying by not returning a slice

--- src/main.rs
+++ src/main.rs
@@ -26,44 +26,44 @@ enum CodeGeneratorException {}
 
 // thank you, reru!
 struct TypeCollector {
-  seen: HashSet<String>,
-  elements: Vec<DescriptorProto>,
+    seen: HashSet<String>,
+    elements: Vec<DescriptorProto>,
 }
 
 impl TypeCollector {
-  fn recurse(&mut self, mut descriptor: DescriptorProto, prefix: &str) {
-      let absolute_name = format!("{prefix:}.{:}", descriptor.name());
-      if self.seen.contains(&absolute_name) {
-          return;
-      }
+    fn recurse(&mut self, mut descriptor: DescriptorProto, prefix: &str) {
+        let absolute_name = format!("{prefix:}.{:}", descriptor.name());
+        if self.seen.contains(&absolute_name) {
+            return;
+        }
 
-      let children = std::mem::take(&mut descriptor.nested_type);
+        let children = std::mem::take(&mut descriptor.nested_type);
 
-      self.seen.insert(absolute_name.clone());
-      self.elements.push(descriptor);
+        self.seen.insert(absolute_name.clone());
+        self.elements.push(descriptor);
 
-      for child in children {
-          self.recurse(child, &absolute_name);
-      }
-  }
-
-  fn run(&mut self, file: FileDescriptorProto, prefix: &str) -> &[DescriptorProto] {
-      self.seen.clear();
-      self.elements.clear();
-
-      for ty in file.message_type {
-        self.recurse(ty, prefix);
-      }
-
-      &self.elements
-  }
-
-  fn new() -> Self {
-    Self {
-      seen: HashSet::new(),
-      elements: Vec::new()
+        for child in children {
+            self.recurse(child, &absolute_name);
+        }
     }
-  }
+
+    fn run(&mut self, file: FileDescriptorProto, prefix: &str) -> Vec<DescriptorProto> {
+        self.seen.clear();
+        self.elements.clear();
+
+        for ty in file.message_type {
+            self.recurse(ty, prefix);
+        }
+
+        self.elements
+    }
+
+    fn new() -> Self {
+        Self {
+            seen: HashSet::new(),
+            elements: Vec::new(),
+        }
+    }
 }
 
 #[derive(Debug, Default)]
@@ -88,19 +88,18 @@ impl<'a> CodeGenerator<'a> {
         let mut type_collector = TypeCollector::new();
         let mut types: Vec<DescriptorProto> = Vec::new();
         for file in &request.proto_file {
-          let package_prefix = if let Some(package) = &file.package {
-              format!(".{package:}")
-          } else {
-              "".to_string()
-          };
+            let package_prefix = match &file.package {
+                Some(package) => format!(".{package:}"),
+                None => "".to_string(),
+            };
 
-          // clone is necessary here :-(
-          let file_types = type_collector.run(file.clone(), &package_prefix);
-          types.extend_from_slice(file_types)
+            // clone is necessary here :-(
+            let file_types = type_collector.run(file.clone(), &package_prefix);
+            types.extend(file_types);
         }
 
         for ty in types {
-          dbg!(ty.name);
+            dbg!(ty.name);
         }
 
         Self {
@@ -117,20 +116,19 @@ impl<'a> CodeGenerator<'a> {
             .map(|file| self.generate_file(file))
             .try_collect();
 
-        if let Err(err) = files {
-            CodeGeneratorResponse {
+        match files {
+            Err(err) => CodeGeneratorResponse {
                 error: Some(format!("{err:?}")),
                 file: Vec::default(),
                 supported_features: None,
                 special_fields: SpecialFields::default(),
-            }
-        } else {
-            CodeGeneratorResponse {
+            },
+            Ok(files) => CodeGeneratorResponse {
                 error: None,
+                file: files,
                 supported_features: None,
-                file: files.unwrap(),
                 special_fields: SpecialFields::default(),
-            }
+            },
         }
     }
 
